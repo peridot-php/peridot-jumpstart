9f22fe3cddd0f3b8394e25e902a75a5e97e96016
Brian Scaturro <scaturrob@gmail.com>
2015-01-16 11:41:11 -0500

    put peridot file in src dir

--- src/Hook.php
+++ src/Hook.php
@@ -8,11 +8,11 @@ class Hook
     public static function copyPeridotFile(Event $event)
     {
         $io = $event->getIO();
-        $src = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'peridot.php';
-        $dest = getcwd() . DIRECTOR_SEPARATOR . 'peridot.php';
+        $src = __DIR__ . DIRECTORY_SEPARATOR . 'peridot.php';
+        $dest = getcwd() . DIRECTORY_SEPARATOR . 'peridot.php';
 
-        $io->write("Attempting to copy peridot.php file");
+        $io->write("<info>Copying peridot.php file</info>");
 
-        copy($source, $dest);
+        copy($src, $dest);
     }
 }
