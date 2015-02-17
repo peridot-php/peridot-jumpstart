9fad465456f3fa821c8b75ac217fd71e89e1a989
Brian Scaturro <scaturrob@gmail.com>
2015-02-17 12:34:28 -0500

    include peridot-concurrency

--- peridot.php
+++ peridot.php
@@ -5,11 +5,22 @@ use Peridot\Plugin\Watcher\WatcherPlugin;
 use Peridot\Reporter\CodeCoverageReporters;
 use Peridot\Reporter\Dot\DotReporterPlugin;
 use Peridot\Reporter\ListReporter\ListReporterPlugin;
+use Peridot\Concurrency\ConcurrencyPlugin;
+use Symfony\Component\Console\Input\InputInterface;
 
 return function(EventEmitterInterface $emitter) {
     $watcher = new WatcherPlugin($emitter);
     $dot = new DotReporterPlugin($emitter);
     $list = new ListReporterPlugin($emitter);
+    $concurrency = new ConcurrencyPlugin($emitter);
+
+    // disable watcher for concurrency workers
+    $emitter->on('peridot.execute', function (InputInterface $input) {
+        $token = getenv('PERIDOT_TEST_TOKEN');
+        if ($token) {
+            $input->setOption('watch', false);
+        }
+    });
 
     $coverage = new CodeCoverageReporters($emitter);
     $coverage->register();
